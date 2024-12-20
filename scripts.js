f5b14b935b8560a027aa75e20150a2e18102d321
hassan <hassanjaved0195@gmail.com>
2024-12-20 17:45:10 +0500

    done

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// for loop:
+// for(i=0; i<9; i++){
+//     console.log(i)
+//     document.write(i)
+// }
+//while loop;
+let i=0;
+    while(i<20){
+    console.log(i)
+    i++
+}
+
+//do while loop;
+let i=0
+do{i}
+while (i=0; i<100)
+    console.log(i++)
